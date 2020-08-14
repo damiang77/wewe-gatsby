226c1fb84eff6086ec710268bb81a8b7dab4fbea
Damian Garbera <damiangarbera@gmail.com>
2020-08-14 15:06:17 +0200

    updated video loading

--- src/components/offers/offers.js
+++ src/components/offers/offers.js
@@ -20,7 +20,6 @@ const Heading = styled.h2`
   font-size: 48px;
   font-weight: 700;
   color: #fff;
-  
 `
 const Description = styled.p`
   font-family: "Lato", sans-serif;
@@ -41,7 +40,8 @@ const Button = styled.div`
   color: ${props => (props.active ? "yellow" : "white")};
   margin: 1rem 0 1rem 0;
   font-weight: 400;
-  background: ${props => (props.active ? "rgba(255,255,255,0.1)" : "transparent")};
+  background: ${props =>
+    props.active ? "rgba(255,255,255,0.1)" : "transparent"};
 
   &:hover {
     color: yellow;
@@ -63,7 +63,137 @@ const VideoWrapper = styled.div`
 
 const Offers = () => {
   const [chosenVideo, setChosenVideo] = useState(0)
+  const [video, setVideo] = useState(organizacjaVideo)
 
+  const renderSwitchVideo = chosenVideo => {
+    let component
+    switch (chosenVideo) {
+      case 0:
+        component = <Video key="organizacja" src={organizacjaVideo} />
+        break
+      case 1:
+        component = <Video key="animacja" src={animacjaVideo} />
+        break
+      case 2:
+        component = <Video key="storyboard" src={storyboardVideo} />
+        break
+      case 3:
+        component = <Video key="foto" src={fotoVideo} />
+        break
+      case 4:
+        component = <Video key="cgi" src={cgiVideo} />
+        break
+      case 5:
+        component = <Video key="social" src={socialVideo} />
+        break
+      case 7:
+        component = <Video key="dron" src={dronVideo} />
+        break
+      default:
+        return "Something went wrong. Please reload the page."
+    }
+    return component
+  }
+
+  return (
+    <div>
+      <StyledBackground>
+        <Container>
+          <Row>
+            <Col>
+              <Heading>Oferta</Heading>
+              <Description>w tym się specjalizujemy</Description>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg="4">
+              <Button
+                onClick={() => {
+                  setChosenVideo(0)
+                  setVideo(organizacjaVideo)
+                }}
+                active={chosenVideo === 0}
+              >
+                Organizacja planów filmowych i zdjęciowych
+              </Button>
+              <Button
+                onClick={() => {
+                  setChosenVideo(1)
+                  setVideo(animacjaVideo)
+                }}
+                active={chosenVideo === 1}
+              >
+                Animacja 2D i ilustracje
+              </Button>
+              <Button
+                onClick={() => {
+                  setChosenVideo(2)
+                  setVideo(storyboardVideo)
+                }}
+                active={chosenVideo === 2}
+              >
+                Storyboardy
+              </Button>
+              <Button
+                onClick={() => {
+                  setChosenVideo(3)
+                  setVideo(fotoVideo)
+                }}
+                active={chosenVideo === 3}
+              >
+                Fotografia
+              </Button>
+              <Button
+                onClick={() => {
+                  setChosenVideo(4)
+                  setVideo(cgiVideo)
+                }}
+                active={chosenVideo === 4}
+              >
+                Animacje 3D i CGI
+              </Button>
+              <Button
+                onClick={() => {
+                  setChosenVideo(5)
+                  setVideo(socialVideo)
+                }}
+                active={chosenVideo === 5}
+              >
+                Social Media Content
+              </Button>
+              <Button
+                onClick={() => {
+                  setChosenVideo(6)
+                }}
+                active={chosenVideo === 6}
+              >
+                Live stream
+              </Button>
+              <Button
+                onClick={() => {
+                  setChosenVideo(7)
+                  setVideo(dronVideo)
+                }}
+                active={chosenVideo === 7}
+              >
+                Ujęcia z drona
+              </Button>
+            </Col>
+            <Col>
+              <VideoWrapper>
+                <Video src={video} />
+              </VideoWrapper>
+            </Col>
+          </Row>
+        </Container>
+      </StyledBackground>
+    </div>
+  )
+}
+
+export default Offers
+
+/*
   const renderSwitchVideo = chosenVideo => {
     let component
     switch (chosenVideo) {
@@ -94,75 +224,4 @@ const Offers = () => {
     return component
   }
 
-  return (
-    <div>
-      <StyledBackground>
-        <Container>
-          <Row>
-            <Col>
-              <Heading>Oferta</Heading>
-              <Description>w tym się specjalizujemy</Description>
-            </Col>
-          </Row>
-          <Row>
-            <Col lg="4">
-              <Button
-                onClick={() => setChosenVideo(0)}
-                active={chosenVideo === 0}
-              >
-                Organizacja planów filmowych i zdjęciowych
-              </Button>
-              <Button
-                onClick={() => setChosenVideo(1)}
-                active={chosenVideo === 1}
-              >
-                Animacja 2D i ilustracje
-              </Button>
-              <Button
-                onClick={() => setChosenVideo(2)}
-                active={chosenVideo === 2}
-              >
-                Storyboardy
-              </Button>
-              <Button
-                onClick={() => setChosenVideo(3)}
-                active={chosenVideo === 3}
-              >
-                Fotografia
-              </Button>
-              <Button
-                onClick={() => setChosenVideo(4)}
-                active={chosenVideo === 4}
-              >
-                Animacje 3D i CGI
-              </Button>
-              <Button
-                onClick={() => setChosenVideo(5)}
-                active={chosenVideo === 5}
-              >
-                Social Media Content
-              </Button>
-              <Button
-                onClick={() => setChosenVideo(6)}
-                active={chosenVideo === 6}
-              >
-                Live stream
-              </Button>
-              <Button
-                onClick={() => setChosenVideo(7)}
-                active={chosenVideo === 7}
-              >
-                Ujęcia z drona
-              </Button>
-            </Col>
-            <Col>
-              <VideoWrapper>{renderSwitchVideo(chosenVideo)}</VideoWrapper>
-            </Col>
-          </Row>
-        </Container>
-      </StyledBackground>
-    </div>
-  )
-}
-
-export default Offers
+  */
