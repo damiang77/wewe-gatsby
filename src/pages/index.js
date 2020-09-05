ae0e2a36458375f24273f7b09e476f0408c5ed7f
Damian Garbera <damiangarbera@gmail.com>
2020-09-05 18:20:37 +0200

    buttons selection fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,34 @@
-import React, { useEffect, createRef, useRef } from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Videos from "../components/videos/videos"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col} from 'react-bootstrap';
-import Menu from "../components/menu";
-import About from "../components/about";
-import HeroVideo from "../components/HeroVideo/HeroVideo";
-import Companies from "../components/companies";
-import Offers from "../components/offers/offers";
+import Videos2 from "../components/videos/videos2"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Menu from "../components/menu"
+import About from "../components/about"
+import HeroVideo from "../components/HeroVideo/HeroVideo"
+import Companies from "../components/companies"
+import Offers from "../components/offers/offers"
 import Portfolio from "../components/portfolio/portfolio"
 
 const IndexPage = () => {
- 
-
-  useEffect(() => {
-  
-    
-  }, [])
+  useEffect(() => {}, [])
 
   return (
-<>
-  <Menu/>
-  <HeroVideo/>
-  <About/>
-  <Companies/>
- <Videos/>
-
-</>
+    <>
+      <Menu />
+      <HeroVideo />
+      <About />
+      <Companies />
+      <Offers/>
+      <Videos2 />
+    </>
   )
 }
 
-export default IndexPage;
+export default IndexPage
 
 /*
 
