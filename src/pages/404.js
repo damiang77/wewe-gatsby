12f6358a2f57fd6b3417d2a5d013bb7f7fe0dc5e
Damian Garbera <damiangarbera@gmail.com>
2020-10-04 21:49:36 +0200

    custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,28 @@
 import React from "react"
-
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {Link} from "gatsby"
+
+const ErrorWrapper = styled.div`
+width:100%;
+height: 100vh;
+background: black;
+color: white;
+display:flex;
+flex-flow:column;
+justify-content: center;
+align-items: center;
+`
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <ErrorWrapper>
+    <h1>404 - strony nie znaleziono</h1>
+    <Link to="/"><h2>Strona główna</h2></Link>
+    </ErrorWrapper>
+
   </Layout>
 )
 
