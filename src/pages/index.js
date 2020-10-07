7952efb9168c3564009d01685ea801f9efada933
Damian Garbera <damiangarbera@gmail.com>
2020-10-07 11:32:21 +0200

    added seo component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,24 +6,22 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Videos from "../components/videos/videos"
 import "bootstrap/dist/css/bootstrap.min.css"
-import Menu from "../components/menu"
 import About from "../components/about"
 import HeroVideo from "../components/HeroVideo/heroVideo"
 import Companies from "../components/companies"
 import Offers from "../components/offers/offers"
-import HowWeWork from "../components/howWeWork/howWeWork"
 import WorkSteps from "../components/WorkSteps/WorkSteps"
-import MessageModal from "../components/messageModal"
 import VideoPlayer from "../components/videoPlayer"
 import TeamComponent from "../components/team/TeamComponent"
 import ContactForm from "../components/contactForm"
-import Footer from "../components/footer"
+
 
 const IndexPage = () => {
   useEffect(() => {}, [])
 
   return (
     <Layout>
+      <SEO title="wewe" description="" />
       <VideoPlayer/>
       <HeroVideo />
       <About />
