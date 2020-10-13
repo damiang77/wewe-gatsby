0f049a89278d792aae844f9085278a4e808eaa38
Damian Garbera <damiangarbera@gmail.com>
2020-10-13 20:36:25 +0200

    title, autor change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `wewe films`,
+    description: ``,
+    author: `damiangarbera@gmail.com`,
   },
   plugins: [`gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
